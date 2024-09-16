fda65ad8a7b6f49494e2c92612ad388b6f484777
João Lucas <jony.lucas.br13@gmail.com>
2024-09-15 21:09:53 -0300

    Player Physics Control + Initial Level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,24 @@
-extends Node3D
+extends RigidBody3D
 
 @export var boost_speed: float = 10.0
 @export var turn_speed: float = 20.0
+var apply_boost: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		translate(Vector3(0, boost_speed * delta, 0))
+		apply_boost = true
 	
 	if Input.is_action_pressed("turn_right"):
-		rotate_z(deg_to_rad(-turn_speed * delta))
+		apply_torque(Vector3(0, 0, -turn_speed * delta))
+		# rotate_z(deg_to_rad(-turn_speed * delta))
 	
 	if Input.is_action_pressed("turn_left"):
-		rotate_z(deg_to_rad(turn_speed * delta))
+		apply_torque(Vector3(0, 0, turn_speed * delta))
+		# rotate_z(deg_to_rad(turn_speed * delta))
 
+func _physics_process(delta: float) -> void:
+	if apply_boost:
+		var boost = transform.basis.y * boost_speed * delta # 3D Nodes stores rotation in basis
+		apply_central_force(boost)
+		apply_boost = false
