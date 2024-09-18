b976da6f957a5adb83bc56e7fe7a65857a3c1591
João Lucas <jony.lucas.br13@gmail.com>
2024-09-17 23:37:30 -0300

    Goal Logic + New Levels

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,11 @@
 extends RigidBody3D
 
+## Vertical force applied to the player
 @export var boost_speed: float = 10.0
+
+## Angular speed of the player
 @export var turn_speed: float = 20.0
+
 var apply_boost: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +26,10 @@ func _physics_process(delta: float) -> void:
 		var boost = transform.basis.y * boost_speed * delta # 3D Nodes stores rotation in basis
 		apply_central_force(boost)
 		apply_boost = false
+
+func _on_body_entered(body:Node) -> void:
+	if body.is_in_group("goal"):
+		get_tree().change_scene_to_file(body.next_level)
+	if body.is_in_group("obstacle"):
+		get_tree().reload_current_scene()
+		
